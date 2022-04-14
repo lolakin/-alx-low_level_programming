0de0d621c52b187e3507b42116188d4dc655d74c
lolakin <lois7lade@gmail.com>
2022-04-14 09:56:32 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers 0 to 9
+ * Returns: nothing
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n + '0');
+		n += 1;
+	}
+	_putchar('\n');
+}
