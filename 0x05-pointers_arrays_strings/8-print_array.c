75ad067ada2ff73a4894dbcba54591cc76d20363
lolakin <lois7lade@gmail.com>
2022-04-20 11:45:41 -0700

    My first commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers followed by a new line
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
  * @a: array to be printed
  * @n: number of elements to print
  */
