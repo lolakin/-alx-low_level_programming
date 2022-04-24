1ad147436175992de5b3faaf904f46362d2053e7
lolakin <lois7lade@gmail.com>
2022-04-24 15:16:34 -0700

    My first commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,9 +17,7 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n
-	 * main - add one line to this code, so that the program prints a[2] = 98,
-	 * followed by a new line. */
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
