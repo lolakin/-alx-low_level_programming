3839f45260cdef656ddec207b2461a0ce34b07a1
lolakin <lois7lade@gmail.com>
2022-04-25 00:48:30 -0700

    My first commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - a function that adds one line to the code, so that the program prints
+ * a[2] = 98, followed by a new line.
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - you are not allowed to code anything else than this line of code
+ */
 int main(void)
 {
 	int n;
@@ -8,14 +16,6 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
