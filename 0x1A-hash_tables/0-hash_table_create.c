b44eab8ac41edd7e2086fadc845de7c6080dbcd9
Lois Lade <96300947+lolakin@users.noreply.github.com>
2022-07-29 12:54:58 +0100

    My commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,11 +2,13 @@
  * Akinrele Lois
  */
 
+#include "hash_tables.h"
+
 /**
  * hash_table_create - creates a hash table
  * @size: the size of the array
  *
- * Return: A pointer to the newly created hash table
+ *Return: A pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
